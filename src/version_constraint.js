64dcfd0a82fbe2bcba9ac342399389a88b016ef5
mikestone <stonemj@gmail.com>
2012-06-19 14:50:24 -0700

    Initial implementation of parseVersion and compareVersion, and exact version comparison.

--- src/version_constraint.js
+++ src/version_constraint.js
@@ -2,6 +2,40 @@
  * Documentation coming soon....
  */
 (function($) {
-    $.version = function(constraint) {
+    $.parseVersion = function(version) {
+        if (version == "") {
+            return [];
+        }
+
+        return $.map(version.split("."), function(x) { return parseInt(x, 10); });
+    };
+
+    $.compareVersion = function(a, b) {
+        var parsedA = $.parseVersion(a);
+        var parsedB = $.parseVersion(b);
+
+        for (var i = 0; i < Math.min(parsedA.length, parsedB.length); i++) {
+            if (parsedA[i] < parsedB[i]) {
+                return -1;
+            }
+
+            if (parsedA[i] > parsedB[i]) {
+                return 1;
+            }
+        }
+
+        if (parsedA.length < parsedB.length) {
+            return -1;
+        }
+
+        if (parsedA.length > parsedB.length) {
+            return 1;
+        }
+
+        return 0;
+    };
+
+    $.version = function(constraint, version) {
+        return $.compareVersion(constraint, version) === 0;
     };
 })(jQuery);
